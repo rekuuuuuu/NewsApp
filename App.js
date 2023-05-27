da898567d22f96aa8643f7c8fdc2a56c4935e243
Rkkk <67547614+rekuuuuuu@users.noreply.github.com>
2023-05-27 21:36:03 +0900

    NewsAPI in App.js

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
+import { useState, useEffect } from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, SafeAreaView, FlatList } from "react-native";
 import { ListItem } from "./components/ListItem";
-import articles from "./dummies/articles";
+import axios from "axios";
+import Constants from "expo-constants";
 
+const URL = `https://newsapi.org/v2/top-headlines?country=jp&category=business&apiKey=${Constants.manifest.extra.newsApiKey}`;
 export default function App() {
+  const [articles, setArticles] = useState([]);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      console.log(response.data.articles);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <FlatList data={articles} renderItem={({ item }) => <ListItem imageUrl={item.urlToImage} title={item.title} author={item.author}></ListItem>} keyExtractor={(item, index) => index.toString()}></FlatList>
